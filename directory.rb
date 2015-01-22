4345d2586055d1ed2b857e279ebaa32e3e72e81a
Meads <meads109@gmail.com>
2015-01-22 16:29:11 +0000

    setup steps for student directory is done. Can take user input and create an array of students and print outhe student name and total count

--- directory.rb
+++ directory.rb
@@ -1,19 +1,6 @@
-#lets puta all the students in an array 
-students = [
-	{:name => "Dr Hannibal Lecter", :cohort => :november},
-	{:name => "Darth Vader", :cohort => :november},
-	{:name => "Nurse Ratchet", :cohort => :november},
-	{:name => "Michael Corleone", :cohort => :november},
-	{:name => "Alex De Large", :cohort => :november},
-	{:name => "The Alien", :cohort => :november},
-	{:name => "The Terminator", :cohort => :november},
-	{:name => "Freddy Kruger", :cohort => :november},
-	{:name => "The Joker", :cohort => :november}
-	]
-
 def print_header
-puts  "The students of my cohort at Makers Academy"
-puts "-------------------------"
+	puts  "The students of my cohort at Makers Academy"
+	puts "-------------------------"
 end	
 
 def print(students)
@@ -27,10 +14,26 @@ def print_footer(names)
 	puts "Overall we have #{names.length} great students"
 end
 
-#nothing happens untill we call the method
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit the return key twice"
+	#create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty repeat this code
+	while !name.empty? do
+		#add the student has to the array
+		students << {:name => name, :cohort => 'november'}
+		puts "now we have #{students.length} students"
+		#get another name from the user
+		name = gets.chomp
+	end
+	#returns the array of students
+	students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
-
-
-
